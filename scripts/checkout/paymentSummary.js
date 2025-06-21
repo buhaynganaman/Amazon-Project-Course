1100a306d7157b1a44cf5d535faa13c015ed88bd
buhaynganaman <williamrextaglocop@gmail.com>
2025-06-21 20:10:47 +0800

    Create Order using the backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,66 +2,100 @@ import { cart } from "../../data/cart-class.js";
 import { getProduct } from '../../data/products.js';
 import { formatCurrency, calculate10PercentTax } from '../utils/money format/money.js';
 import { delivery } from '../../data/deliveryOptions.js';
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary() {
 
-const cartQuantity = cart.calculateCartQuantity(); // get the Over All quantity
-let totalProductsPriceCents = 0;
-let totalShippingPriceCents = 0;
+	const cartQuantity = cart.calculateCartQuantity(); // get the Over All quantity
+	let totalProductsPriceCents = 0;
+	let totalShippingPriceCents = 0;
 
-cart.cartItem.forEach(cartItem => {
-	const productId = cartItem.productId;
-	const matchingProduct = getProduct(productId); // Find matching product
+	cart.cartItem.forEach(cartItem => {
+		const productId = cartItem.productId;
+		const matchingProduct = getProduct(productId); // Find matching product
 
-	if (matchingProduct) { // Make sure the product exists
-	totalProductsPriceCents += matchingProduct.priceCents * cartItem.quantity; // multiply products price to its quantity
-}
+		if (matchingProduct) { // Make sure the product exists
+			// multiply products price to its quantity
+			totalProductsPriceCents += matchingProduct.priceCents * cartItem.quantity;
+		}
 
-	const deliveryOption = delivery.getDeliveryOption(cartItem.deliveryOptionsId); // Find matching delivery option
-	totalShippingPriceCents += deliveryOption.shippingPriceCents; // store shipping price
-});
+		// Find matching delivery option
+		const deliveryOption = delivery.getDeliveryOption(cartItem.deliveryOptionsId);
+		totalShippingPriceCents += deliveryOption.shippingPriceCents; // store shipping price
+	});
+
 	// sum the total product price to shipping price
-const totalBeforeTaxCents = Number(totalProductsPriceCents + totalShippingPriceCents);
+	const totalBeforeTaxCents = Number(totalProductsPriceCents + totalShippingPriceCents);
 	// hold the 10% tax of the total price
-const totalTaxCents = Number(calculate10PercentTax(totalBeforeTaxCents));
+	const totalTaxCents = Number(calculate10PercentTax(totalBeforeTaxCents));
 	// sum the total price to 10% tax
-const finalTotalPriceCents = totalBeforeTaxCents + totalTaxCents;
+	const finalTotalPriceCents = totalBeforeTaxCents + totalTaxCents;
 
-const paymentSummaryHTML = 
-`     
-	<div class="payment-summary-title">
-		Order Summary
-	</div>
+	const paymentSummaryHTML = 
+	`     
+		<div class="payment-summary-title">
+			Order Summary
+		</div>
 
-	<div class="payment-summary-row">
-		<div>Items (${cartQuantity}):</div>
-		<div class="payment-summary-money">$${formatCurrency(totalProductsPriceCents)}</div>
-	</div>
+		<div class="payment-summary-row">
+			<div>Items (${cartQuantity}):</div>
+			<div class="payment-summary-money">$${formatCurrency(totalProductsPriceCents)}</div>
+		</div>
 
-	<div class="payment-summary-row">
-		<div>Shipping &amp; handling:</div>
-		<div class="payment-summary-money">$${formatCurrency(totalShippingPriceCents)}</div>
-	</div>
+		<div class="payment-summary-row">
+			<div>Shipping &amp; handling:</div>
+			<div class="payment-summary-money">$${formatCurrency(totalShippingPriceCents)}</div>
+		</div>
 
-	<div class="payment-summary-row subtotal-row">
-		<div>Total before tax:</div>
-		<div class="payment-summary-money">$${formatCurrency(totalBeforeTaxCents)}</div>
-	</div>
+		<div class="payment-summary-row subtotal-row">
+			<div>Total before tax:</div>
+			<div class="payment-summary-money">$${formatCurrency(totalBeforeTaxCents)}</div>
+		</div>
 
-	<div class="payment-summary-row">
-		<div>Estimated tax (10%):</div>
-		<div class="payment-summary-money">$${formatCurrency(totalTaxCents)}</div>
-	</div>
+		<div class="payment-summary-row">
+			<div>Estimated tax (10%):</div>
+			<div class="payment-summary-money">$${formatCurrency(totalTaxCents)}</div>
+		</div>
 
-	<div class="payment-summary-row total-row">
-		<div>Order total:</div>
-		<div class="payment-summary-money">$${formatCurrency(finalTotalPriceCents)}</div>
-	</div>
+		<div class="payment-summary-row total-row">
+			<div>Order total:</div>
+			<div class="payment-summary-money">$${formatCurrency(finalTotalPriceCents)}</div>
+		</div>
 
-	<button class="place-order-button button-primary">
-		Place your order
-	</button>
-`
-document.querySelector('.js-paymentSummary').innerHTML = paymentSummaryHTML;
+		<button class="place-order-button button-primary js-PlaceOrderBtn">
+			Place your order
+		</button>
+	`
+	document.querySelector('.js-paymentSummary').innerHTML = paymentSummaryHTML;
+
+	document.querySelector('.js-PlaceOrderBtn')
+	.addEventListener('click', async () => {
+
+		try {
+
+			const response = await fetch('https://supersimplebackend.dev/orders', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					cart: cart.getItems()
+				}),
+			});
+	
+			const order = await response.json();
+			addOrder(order);
+			console.log('Place Order Success')
+
+		} catch (error) {
+			console.log('ERROR "POST-REQUEST" at place order', error)
+		}
+
+		window.location.href = 'orders.html';
+
+	});
+
+
+// console.log(cart.getItems())
 // console.log("paymentSummary.js All Working"); // for checking
 }
